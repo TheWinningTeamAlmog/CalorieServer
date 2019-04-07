2d9fff677e02727dfc1d9ae74c782dcb059a564a
liorbond <liorbond@gmail.com>
2019-04-07 21:13:56 +0300

    bla

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from instagram_scraper.app import InstagramScraper
 from selenium import webdriver
-import argparse
+import json
+from flask import Flask
 
 def get_followers(username):
     # download html
@@ -19,7 +20,8 @@ def get_followers(username):
     followers_pos = content.find(followers_start)
     followers_pos_start = content.find("content", followers_pos) + len("content=\"")
     followers_pos_end   = content.find(" ", followers_pos_start)
-    print(int(content[followers_pos_start:followers_pos_end]))
+
+    return int(content[followers_pos_start:followers_pos_end])
 
 def get_full_info(username):
     output_path = "/tmp/inst_scraper/"
@@ -38,28 +40,25 @@ def get_full_info(username):
     json_path = output_path + username
     
 def get_avg_likes(username):
+    return 112
 
-
-def get_user_metadata(usrname):
+@app.route("/")
+def get_user_metadata(username):
     followers = get_followers(username)
+    avg_likes = get_avg_likes(username)
+
+    data = {}
+    data['name']      = username
+    data['followers'] = followers
+    data['avg_likes'] = avg_likes
+
+    print(json.dumps(data)) 
 
 
 
 
 def main():
-    get_followers("netaalon22")
+    app = Flask(__name__)
+    get_user_metadata("netaalon22")
 
 main()
-
-
-{
-        "name": "\\",
-        "followers": "\\number\\",
-        "photos": [ { 
-                "image" : "\\base64\\",
-                "comments" : [""],
-                "description": "",
-                "tags": [""],
-                "likes": "\\number\\"
-        } ]
-}
